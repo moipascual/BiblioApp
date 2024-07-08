165e286799d4fe9648e774a75ffe2355b0af8415
moipascual <mpascual.laguna@gmail.com>
2024-07-08 15:49:09 -0500

    Primer commit

---
+++ src/test/java/com/aluracursos/biblioApp/BiblioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.biblioApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
